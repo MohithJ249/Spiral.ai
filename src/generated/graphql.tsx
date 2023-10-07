22ead331512089259c703770c71b18294e4185f5
MatsalVistry <vatsalmistry4796@gmail.com>
2023-10-07 17:14:50 -0500

    added all graphql mutations

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -131,6 +131,70 @@ export type User = {
   username: Scalars['String']['output'];
 };
 
+export type AddCollaboratorMutationVariables = Exact<{
+  scriptid: Scalars['ID']['input'];
+  email: Scalars['String']['input'];
+}>;
+
+
+export type AddCollaboratorMutation = { __typename?: 'Mutation', addCollaborator?: boolean | null };
+
+export type CreateScriptMutationVariables = Exact<{
+  userid: Scalars['ID']['input'];
+  title: Scalars['String']['input'];
+  s3Link: Scalars['String']['input'];
+}>;
+
+
+export type CreateScriptMutation = { __typename?: 'Mutation', createScript?: { __typename?: 'Script', s3link: string, scriptid: string, title: string } | null };
+
+export type CreateScriptVersionMutationVariables = Exact<{
+  scriptid: Scalars['ID']['input'];
+  s3Link: Scalars['String']['input'];
+}>;
+
+
+export type CreateScriptVersionMutation = { __typename?: 'Mutation', createScriptVersion?: { __typename?: 'ScriptVersion', s3link: string, scriptid: string, time_saved: string } | null };
+
+export type CreateUserMutationVariables = Exact<{
+  username: Scalars['String']['input'];
+  email: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+}>;
+
+
+export type CreateUserMutation = { __typename?: 'Mutation', createUser?: { __typename?: 'User', email: string, password: string, username: string, userid: string } | null };
+
+export type DeleteScriptMutationVariables = Exact<{
+  scriptid: Scalars['ID']['input'];
+}>;
+
+
+export type DeleteScriptMutation = { __typename?: 'Mutation', deleteScript?: boolean | null };
+
+export type DeleteUserMutationVariables = Exact<{
+  userid: Scalars['ID']['input'];
+}>;
+
+
+export type DeleteUserMutation = { __typename?: 'Mutation', deleteUser?: boolean | null };
+
+export type RemoveCollaboratorMutationVariables = Exact<{
+  scriptid: Scalars['ID']['input'];
+  email: Scalars['ID']['input'];
+}>;
+
+
+export type RemoveCollaboratorMutation = { __typename?: 'Mutation', removeCollaborator?: boolean | null };
+
+export type UpdateScriptTitleMutationVariables = Exact<{
+  scriptid: Scalars['ID']['input'];
+  title: Scalars['String']['input'];
+}>;
+
+
+export type UpdateScriptTitleMutation = { __typename?: 'Mutation', updateScriptTitle?: { __typename?: 'Script', title: string, s3link: string } | null };
+
 export type GetAllSharedScriptsQueryVariables = Exact<{
   userid: Scalars['ID']['input'];
 }>;
@@ -166,6 +230,278 @@ export type LoginQueryVariables = Exact<{
 export type LoginQuery = { __typename?: 'Query', login?: { __typename?: 'User', email: string, userid: string, username: string } | null };
 
 
+export const AddCollaboratorDocument = gql`
+    mutation AddCollaborator($scriptid: ID!, $email: String!) {
+  addCollaborator(scriptid: $scriptid, email: $email)
+}
+    `;
+export type AddCollaboratorMutationFn = Apollo.MutationFunction<AddCollaboratorMutation, AddCollaboratorMutationVariables>;
+
+/**
+ * __useAddCollaboratorMutation__
+ *
+ * To run a mutation, you first call `useAddCollaboratorMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useAddCollaboratorMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [addCollaboratorMutation, { data, loading, error }] = useAddCollaboratorMutation({
+ *   variables: {
+ *      scriptid: // value for 'scriptid'
+ *      email: // value for 'email'
+ *   },
+ * });
+ */
+export function useAddCollaboratorMutation(baseOptions?: Apollo.MutationHookOptions<AddCollaboratorMutation, AddCollaboratorMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<AddCollaboratorMutation, AddCollaboratorMutationVariables>(AddCollaboratorDocument, options);
+      }
+export type AddCollaboratorMutationHookResult = ReturnType<typeof useAddCollaboratorMutation>;
+export type AddCollaboratorMutationResult = Apollo.MutationResult<AddCollaboratorMutation>;
+export type AddCollaboratorMutationOptions = Apollo.BaseMutationOptions<AddCollaboratorMutation, AddCollaboratorMutationVariables>;
+export const CreateScriptDocument = gql`
+    mutation CreateScript($userid: ID!, $title: String!, $s3Link: String!) {
+  createScript(userid: $userid, title: $title, s3link: $s3Link) {
+    s3link
+    scriptid
+    title
+  }
+}
+    `;
+export type CreateScriptMutationFn = Apollo.MutationFunction<CreateScriptMutation, CreateScriptMutationVariables>;
+
+/**
+ * __useCreateScriptMutation__
+ *
+ * To run a mutation, you first call `useCreateScriptMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateScriptMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createScriptMutation, { data, loading, error }] = useCreateScriptMutation({
+ *   variables: {
+ *      userid: // value for 'userid'
+ *      title: // value for 'title'
+ *      s3Link: // value for 's3Link'
+ *   },
+ * });
+ */
+export function useCreateScriptMutation(baseOptions?: Apollo.MutationHookOptions<CreateScriptMutation, CreateScriptMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateScriptMutation, CreateScriptMutationVariables>(CreateScriptDocument, options);
+      }
+export type CreateScriptMutationHookResult = ReturnType<typeof useCreateScriptMutation>;
+export type CreateScriptMutationResult = Apollo.MutationResult<CreateScriptMutation>;
+export type CreateScriptMutationOptions = Apollo.BaseMutationOptions<CreateScriptMutation, CreateScriptMutationVariables>;
+export const CreateScriptVersionDocument = gql`
+    mutation CreateScriptVersion($scriptid: ID!, $s3Link: String!) {
+  createScriptVersion(scriptid: $scriptid, s3link: $s3Link) {
+    s3link
+    scriptid
+    time_saved
+  }
+}
+    `;
+export type CreateScriptVersionMutationFn = Apollo.MutationFunction<CreateScriptVersionMutation, CreateScriptVersionMutationVariables>;
+
+/**
+ * __useCreateScriptVersionMutation__
+ *
+ * To run a mutation, you first call `useCreateScriptVersionMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateScriptVersionMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createScriptVersionMutation, { data, loading, error }] = useCreateScriptVersionMutation({
+ *   variables: {
+ *      scriptid: // value for 'scriptid'
+ *      s3Link: // value for 's3Link'
+ *   },
+ * });
+ */
+export function useCreateScriptVersionMutation(baseOptions?: Apollo.MutationHookOptions<CreateScriptVersionMutation, CreateScriptVersionMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateScriptVersionMutation, CreateScriptVersionMutationVariables>(CreateScriptVersionDocument, options);
+      }
+export type CreateScriptVersionMutationHookResult = ReturnType<typeof useCreateScriptVersionMutation>;
+export type CreateScriptVersionMutationResult = Apollo.MutationResult<CreateScriptVersionMutation>;
+export type CreateScriptVersionMutationOptions = Apollo.BaseMutationOptions<CreateScriptVersionMutation, CreateScriptVersionMutationVariables>;
+export const CreateUserDocument = gql`
+    mutation CreateUser($username: String!, $email: String!, $password: String!) {
+  createUser(username: $username, email: $email, password: $password) {
+    email
+    password
+    username
+    userid
+  }
+}
+    `;
+export type CreateUserMutationFn = Apollo.MutationFunction<CreateUserMutation, CreateUserMutationVariables>;
+
+/**
+ * __useCreateUserMutation__
+ *
+ * To run a mutation, you first call `useCreateUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createUserMutation, { data, loading, error }] = useCreateUserMutation({
+ *   variables: {
+ *      username: // value for 'username'
+ *      email: // value for 'email'
+ *      password: // value for 'password'
+ *   },
+ * });
+ */
+export function useCreateUserMutation(baseOptions?: Apollo.MutationHookOptions<CreateUserMutation, CreateUserMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateUserMutation, CreateUserMutationVariables>(CreateUserDocument, options);
+      }
+export type CreateUserMutationHookResult = ReturnType<typeof useCreateUserMutation>;
+export type CreateUserMutationResult = Apollo.MutationResult<CreateUserMutation>;
+export type CreateUserMutationOptions = Apollo.BaseMutationOptions<CreateUserMutation, CreateUserMutationVariables>;
+export const DeleteScriptDocument = gql`
+    mutation DeleteScript($scriptid: ID!) {
+  deleteScript(scriptid: $scriptid)
+}
+    `;
+export type DeleteScriptMutationFn = Apollo.MutationFunction<DeleteScriptMutation, DeleteScriptMutationVariables>;
+
+/**
+ * __useDeleteScriptMutation__
+ *
+ * To run a mutation, you first call `useDeleteScriptMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useDeleteScriptMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [deleteScriptMutation, { data, loading, error }] = useDeleteScriptMutation({
+ *   variables: {
+ *      scriptid: // value for 'scriptid'
+ *   },
+ * });
+ */
+export function useDeleteScriptMutation(baseOptions?: Apollo.MutationHookOptions<DeleteScriptMutation, DeleteScriptMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<DeleteScriptMutation, DeleteScriptMutationVariables>(DeleteScriptDocument, options);
+      }
+export type DeleteScriptMutationHookResult = ReturnType<typeof useDeleteScriptMutation>;
+export type DeleteScriptMutationResult = Apollo.MutationResult<DeleteScriptMutation>;
+export type DeleteScriptMutationOptions = Apollo.BaseMutationOptions<DeleteScriptMutation, DeleteScriptMutationVariables>;
+export const DeleteUserDocument = gql`
+    mutation DeleteUser($userid: ID!) {
+  deleteUser(userid: $userid)
+}
+    `;
+export type DeleteUserMutationFn = Apollo.MutationFunction<DeleteUserMutation, DeleteUserMutationVariables>;
+
+/**
+ * __useDeleteUserMutation__
+ *
+ * To run a mutation, you first call `useDeleteUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useDeleteUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [deleteUserMutation, { data, loading, error }] = useDeleteUserMutation({
+ *   variables: {
+ *      userid: // value for 'userid'
+ *   },
+ * });
+ */
+export function useDeleteUserMutation(baseOptions?: Apollo.MutationHookOptions<DeleteUserMutation, DeleteUserMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<DeleteUserMutation, DeleteUserMutationVariables>(DeleteUserDocument, options);
+      }
+export type DeleteUserMutationHookResult = ReturnType<typeof useDeleteUserMutation>;
+export type DeleteUserMutationResult = Apollo.MutationResult<DeleteUserMutation>;
+export type DeleteUserMutationOptions = Apollo.BaseMutationOptions<DeleteUserMutation, DeleteUserMutationVariables>;
+export const RemoveCollaboratorDocument = gql`
+    mutation RemoveCollaborator($scriptid: ID!, $email: ID!) {
+  removeCollaborator(scriptid: $scriptid, email: $email)
+}
+    `;
+export type RemoveCollaboratorMutationFn = Apollo.MutationFunction<RemoveCollaboratorMutation, RemoveCollaboratorMutationVariables>;
+
+/**
+ * __useRemoveCollaboratorMutation__
+ *
+ * To run a mutation, you first call `useRemoveCollaboratorMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRemoveCollaboratorMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [removeCollaboratorMutation, { data, loading, error }] = useRemoveCollaboratorMutation({
+ *   variables: {
+ *      scriptid: // value for 'scriptid'
+ *      email: // value for 'email'
+ *   },
+ * });
+ */
+export function useRemoveCollaboratorMutation(baseOptions?: Apollo.MutationHookOptions<RemoveCollaboratorMutation, RemoveCollaboratorMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<RemoveCollaboratorMutation, RemoveCollaboratorMutationVariables>(RemoveCollaboratorDocument, options);
+      }
+export type RemoveCollaboratorMutationHookResult = ReturnType<typeof useRemoveCollaboratorMutation>;
+export type RemoveCollaboratorMutationResult = Apollo.MutationResult<RemoveCollaboratorMutation>;
+export type RemoveCollaboratorMutationOptions = Apollo.BaseMutationOptions<RemoveCollaboratorMutation, RemoveCollaboratorMutationVariables>;
+export const UpdateScriptTitleDocument = gql`
+    mutation UpdateScriptTitle($scriptid: ID!, $title: String!) {
+  updateScriptTitle(scriptid: $scriptid, title: $title) {
+    title
+    s3link
+  }
+}
+    `;
+export type UpdateScriptTitleMutationFn = Apollo.MutationFunction<UpdateScriptTitleMutation, UpdateScriptTitleMutationVariables>;
+
+/**
+ * __useUpdateScriptTitleMutation__
+ *
+ * To run a mutation, you first call `useUpdateScriptTitleMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useUpdateScriptTitleMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [updateScriptTitleMutation, { data, loading, error }] = useUpdateScriptTitleMutation({
+ *   variables: {
+ *      scriptid: // value for 'scriptid'
+ *      title: // value for 'title'
+ *   },
+ * });
+ */
+export function useUpdateScriptTitleMutation(baseOptions?: Apollo.MutationHookOptions<UpdateScriptTitleMutation, UpdateScriptTitleMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<UpdateScriptTitleMutation, UpdateScriptTitleMutationVariables>(UpdateScriptTitleDocument, options);
+      }
+export type UpdateScriptTitleMutationHookResult = ReturnType<typeof useUpdateScriptTitleMutation>;
+export type UpdateScriptTitleMutationResult = Apollo.MutationResult<UpdateScriptTitleMutation>;
+export type UpdateScriptTitleMutationOptions = Apollo.BaseMutationOptions<UpdateScriptTitleMutation, UpdateScriptTitleMutationVariables>;
 export const GetAllSharedScriptsDocument = gql`
     query GetAllSharedScripts($userid: ID!) {
   getAllSharedScripts(userid: $userid) {
