b3e97f2ae17de4d7338e2155a11de5f59696cfd4
chai-justin <75556658+justinchai1@users.noreply.github.com>
2023-10-15 22:15:07 -0500

    added styling for create account page

--- src/content/CreateAccount/index.tsx
+++ src/content/CreateAccount/index.tsx
@@ -2,6 +2,8 @@ import '../../components/selectionPage';
 import React, { useState, useEffect } from 'react';
 import { useCreateUserMutation } from '../../generated/graphql';
 import { Button, FormControl, Input, InputLabel, Alert } from '@mui/material';
+import {  TextField, Container, Typography, Box } from '@mui/material';
+import { Lock as LockIcon } from '@mui/icons-material';
 import { ApolloError } from '@apollo/client';
 
 export default function CreateAccountPage() {
@@ -38,25 +40,88 @@ export default function CreateAccountPage() {
     };
 
     return (
-        <div className='create-account-container'>
-            {errorText && <Alert severity="error">{errorText}</Alert>}
-                <br /><br />
-                <FormControl>
-                    <InputLabel>Email</InputLabel>
-                    <Input type='text' value={email} error={errorText?.includes("Please choose a different email")}  onChange={(e) => setEmail(e.target.value)} />
-                </FormControl>
-                <br /><br />
-                <FormControl>
-                    <InputLabel>Username</InputLabel>
-                    <Input type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
-                </FormControl>
-                <br /><br />
-                <FormControl>
-                    <InputLabel>Password</InputLabel>
-                    <Input type='text' value={password} onChange={(e) => setPassword(e.target.value)} />
-                </FormControl>
-                <br /><br />
-                <Button type='submit' disabled={loading} onClick={handleSubmit}>Create Account</Button>
-        </div>
+        <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            mt: 8,
+          }}
+        >
+          <LockIcon color="primary" style={{ fontSize: 40 }} />
+          <Typography component="h1" variant="h5">
+            Create Account
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email} error={errorText?.includes("Please choose a different email")}  onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              value={username} onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password} onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={loading} onClick={handleSubmit}
+            >
+              Create Account
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+        // <div className='create-account-container'>
+        //     {errorText && <Alert severity="error">{errorText}</Alert>}
+        //         <br /><br />
+        //         <FormControl>
+        //             <InputLabel>Email</InputLabel>
+        //             <Input type='text' value={email} error={errorText?.includes("Please choose a different email")}  onChange={(e) => setEmail(e.target.value)} />
+        //         </FormControl>
+        //         <br /><br />
+        //         <FormControl>
+        //             <InputLabel>Username</InputLabel>
+        //             <Input type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+        //         </FormControl>
+        //         <br /><br />
+        //         <FormControl>
+        //             <InputLabel>Password</InputLabel>
+        //             <Input type='text' value={password} onChange={(e) => setPassword(e.target.value)} />
+        //         </FormControl>
+        //         <br /><br />
+        //         <Button type='submit' disabled={loading} onClick={handleSubmit}>Create Account</Button>
+        // </div>
     );
 }
